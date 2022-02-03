41e2acbd2801b1a99f066623c310a8d261f187b1
API Imperfect <alphaogilo@gmail.com>
2022-02-03 17:32:31 +0200

    chore: configure django logging

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -147,3 +147,22 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 CORS_URLS_REGEX = r"^/api/.*$"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
