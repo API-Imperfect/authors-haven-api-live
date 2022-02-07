fae7323a035613dd20b5245dfbf97838061701a8
API Imperfect <alphaogilo@gmail.com>
2022-02-07 17:21:36 +0200

    feat: add celery, redis and flower

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -33,6 +33,7 @@ THIRD_PARTY_APPS = [
     "phonenumber_field",
     "drf_yasg",
     "corsheaders",
+    "djcelery_email",
 ]
 
 LOCAL_APPS = ["core_apps.common", "core_apps.users", "core_apps.profiles"]
@@ -149,6 +150,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_URLS_REGEX = r"^/api/.*$"
 
 AUTH_USER_MODEL = "users.User"
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = env("CELERY_BACKEND")
+CELERY_TIMEZONE = "Africa/Kigali"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 
 LOGGING = {
     "version": 1,
--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -11,3 +11,10 @@ SECRET_KEY = env(
 )
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "info@authors-haven.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Haven"
